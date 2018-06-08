da9ee6d43aea65b0a765f9ab14eec518a25adfef
lguislain <luc.guislain@univ-reims.fr>
2018-06-08 13:35:34 +0200

    Mise à jour

--- esup-pstagedata-web-jsf-servlet/db/create/insert.sql
+++ esup-pstagedata-web-jsf-servlet/db/create/insert.sql
@@ -83,7 +83,7 @@ INSERT INTO `Civilite` (`idCivilite`, `libelleCivilite`) VALUES (1, 'Mlle');
 
 INSERT INTO `Civilite` (`idCivilite`, `libelleCivilite`) VALUES (2, 'Mme');
 
-INSERT INTO `Civilite` (`idCivilite`, `libelleCivilite`) VALUES (3, 'Mr');
+INSERT INTO `Civilite` (`idCivilite`, `libelleCivilite`) VALUES (3, 'M.');
 
 INSERT INTO `Civilite` (`idCivilite`, `libelleCivilite`) VALUES (4, '');
 
